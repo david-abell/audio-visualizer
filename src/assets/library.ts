92fa2c38325c4735fb1c56b292caad8100d570c2
David Abell <79927957+david-abell@users.noreply.github.com>
2022-12-08 11:42:39 +0000

    reorganize album covers

--- src/assets/library.ts
+++ src/assets/library.ts
@@ -5,84 +5,104 @@ import { Track } from "../types/types";
 const library: Track[] = [
   {
     title: "Gotham City",
-    cover: new URL("../assets/TwistedLies.jfif", import.meta.url).href,
+    cover: new URL("../assets/images/covers/TwistedLies.jfif", import.meta.url)
+      .href,
     artist: "Audiorezout",
     url: "GothamCity.mp3",
     id: uuidv4(),
   },
   {
     title: "Hammer War",
-    cover: new URL("../assets/makeyourownway.jpg", import.meta.url).href,
+    cover: new URL(
+      "../assets/images/covers/makeyourownway.jpg",
+      import.meta.url
+    ).href,
     artist: "Audiorezout",
     url: "Hammerwar.mp3",
     id: uuidv4(),
   },
   {
     title: "Sailing Away",
-    cover: new URL("../assets/OrphanedMediaPT2.jfif", import.meta.url).href,
+    cover: new URL(
+      "../assets/images/covers/OrphanedMediaPT2.jfif",
+      import.meta.url
+    ).href,
     artist: "HoliznaCC0",
     url: "SailingAway.mp3",
     id: uuidv4(),
   },
   {
     title: "Tuesday",
-    cover: new URL("../assets/ketsa.jpg", import.meta.url).href,
+    cover: new URL("../assets/images/covers/ketsa.jpg", import.meta.url).href,
     artist: "Ketsa",
     url: "Tuesday.mp3",
     id: uuidv4(),
   },
   {
     title: "Orleans",
-    cover: new URL("../assets/ketsa.jpg", import.meta.url).href,
+    cover: new URL("../assets/images/covers/ketsa.jpg", import.meta.url).href,
     artist: "Ketsa",
     url: "Orleans.mp3",
     id: uuidv4(),
   },
   {
     title: "Upbeat Funk Pop",
-    cover: new URL("../assets/scottholmesmusic.jpg", import.meta.url).href,
+    cover: new URL(
+      "../assets/images/covers/scottholmesmusic.jpg",
+      import.meta.url
+    ).href,
     artist: "Scott Holmes Music",
     url: "UpbeatFunkPop.mp3",
     id: uuidv4(),
   },
   {
     title: "Epic Cinematic",
-    cover: new URL("../assets/CinematicBackground.jpg", import.meta.url).href,
+    cover: new URL(
+      "../assets/images/covers/CinematicBackground.jpg",
+      import.meta.url
+    ).href,
     artist: "Scott Holmes Music",
     url: "EpicCinematic.mp3",
     id: uuidv4(),
   },
   {
     title: "French Girls & Cigarettes",
-    cover: new URL("../assets/streamliner.jpg", import.meta.url).href,
+    cover: new URL("../assets/images/covers/streamliner.jpg", import.meta.url)
+      .href,
     artist: "Mr Smith",
     url: "FrenchGirls%26Cigarettes.mp3",
     id: uuidv4(),
   },
   {
     title: "Pursuit Of Life",
-    cover: new URL("../assets/blueseason3.jpg", import.meta.url).href,
+    cover: new URL("../assets/images/covers/blueseason3.jpg", import.meta.url)
+      .href,
     artist: "Kirk Osamayo",
     url: "PursuitOfLife.mp3",
     id: uuidv4(),
   },
   {
     title: "Blue Skies",
-    cover: new URL("../assets/lifeunfolds.jpg", import.meta.url).href,
+    cover: new URL("../assets/images/covers/lifeunfolds.jpg", import.meta.url)
+      .href,
     artist: "Derek Clegg",
     url: "BlueSkies.mp3",
     id: uuidv4(),
   },
   {
     title: "Master of the Streats",
-    cover: new URL("../assets/urbangeneration.jpg", import.meta.url).href,
+    cover: new URL(
+      "../assets/images/covers/urbangeneration.jpg",
+      import.meta.url
+    ).href,
     artist: "Jonas Hipper",
     url: "MasterOfTheStreets.mp3",
     id: uuidv4(),
   },
   {
     title: "Let's Stay In Love",
-    cover: new URL("../assets/theyears.jpg", import.meta.url).href,
+    cover: new URL("../assets/images/covers/theyears.jpg", import.meta.url)
+      .href,
     artist: "The Years",
     url: "Let'sStayInLove.mp3",
     id: uuidv4(),
