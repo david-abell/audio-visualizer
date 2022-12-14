cdd72e79220818b0af714bf908acbef46ea7afad
David Abell <79927957+david-abell@users.noreply.github.com>
2022-12-14 14:20:40 +0000

    fix: no leading zeroes only for minutes

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,11 +1,14 @@
 const formatTimeString = (num: number) => (num < 10 ? `0${num}` : `${num}`);
 
-export default function formatTime(duration: number, noZeros = false) {
+export default function formatTime(
+  duration: number,
+  noLeadingMinuteZeros = false
+) {
   if (!Number.isFinite(duration)) return 0;
   const minutes = Math.trunc(duration / 60);
   const seconds = Math.trunc(duration - minutes * 60);
-  if (noZeros) {
-    return minutes > 0 ? `${minutes}:${seconds}` : String(seconds);
+  if (noLeadingMinuteZeros) {
+    return `${minutes}:${formatTimeString(seconds)}`;
   }
   return `${formatTimeString(minutes)}:${formatTimeString(seconds)}`;
 }
