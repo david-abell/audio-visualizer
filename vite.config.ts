e22bd8870837a069e7739262d420f0b5ddfb5bcb
David Abell <79927957+david-abell@users.noreply.github.com>
2022-12-15 13:20:59 +0000

    add vitest and React testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
@@ -10,5 +11,8 @@ export default defineConfig({
     modules: {
       localsConvention: "camelCaseOnly",
     },
+  },
+  test: {
+    globals: true,
   },
 });
